a45caccfc25a077b66c0c514ed0135298adf6d37
Tom Chen <tomchen.org@gmail.com>
2020-03-01 14:19:24 +0100

    upd scripts_datatables to newest

--- non_text/scripts_datatables/mmmerge/original/Scripts/Maps/6d03.lua
+++
@@ -1,8 +0,0 @@
------------------------------------------
--- Rescue Sherry Carnegie quest (mm6)
-
-evt.Map[22] = function()
-	if Party.QBits[1703] then
-		NPCFollowers.Add(978)
-	end
-end
--- non_text/scripts_datatables/mmmerge/original/Scripts/Maps/outa2.lua
+++
@@ -1,8 +0,0 @@
-local TileSounds = {[0] = {[0] = 91, 	[1] = 52}}
-
-function events.TileSound(t)
-	local Grp = TileSounds[Game.CurrentTileBin[Map.TileMap[t.X][t.Y]].TileSet]
-	if Grp then
-		t.Sound = Grp[t.Run]
-	end
-end

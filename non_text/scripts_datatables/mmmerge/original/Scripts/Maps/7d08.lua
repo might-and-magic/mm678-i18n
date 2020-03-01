bc07ed2285d84fee9fe1a462cb2597946ee1fe33
Tom Chen <tomchen.org@gmail.com>
2020-03-01 15:25:06 +0100

    folders

--- non_text/scripts_datatables/mmmerge/original/Scripts/Maps/7d08.lua
+++
@@ -1,8 +0,0 @@
-
--- Rescue Loren Steel quest (mm7)
-
-evt.map[376] = function()
-	if Party.QBits[1695] then
-		NPCFollowers.Add(410)
-	end
-end -- Other Loren's parts are in StdQuestsFollowers.lua
--- non_text/scripts_datatables/mmmerge/original/Scripts/Maps/oracle.lua
+++
@@ -1,7 +0,0 @@
-
---~ evt.Map[14] = function()
---~ 	local LightState = not evt.Cmp{"MapVar6", 1}
---~ 	for i,v in Map.Lights do
---~ 		v.Off = LightState
---~ 	end
---~ end

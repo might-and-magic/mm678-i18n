7c2552a41e49d9b009d24eb4f76e1041eb7ef57f
Tom CHEN <tomchen.org@gmail.com>
2020-05-17 17:56:39 +0200

    add zh_TW

--- tools/zhconvert.py
+++ tools/zhconvert.py
@@ -3,7 +3,6 @@
 from pathlib import Path
 from getfilepaths import getFilePaths
 from opencc import OpenCC
-cc = OpenCC('tw2sp')
 
 quoteDict = {
 '「': '“',
@@ -22,10 +21,15 @@ quoteDict = {
 # t2tw: Traditional Chinese to Traditional Chinese (Taiwan standard)
 # tw2s: Traditional Chinese (Taiwan standard) to Simplified Chinese
 # tw2sp: Traditional Chinese (Taiwan standard) to Simplified Chinese (with phrases)
-def convertZhFile(inputPathObj, outputPathObj, method = 'tw2sp', convertQuote = True):
-	f = inputPathObj.open(mode = 'r', newline = '', encoding = None)
+def convertZhFile(inputPathObj, outputPathObj, method = 'tw2sp', convertQuote = True, inputEncoding = 'utf8', outputEncoding = 'utf8', replaceWords = []):
+	f = inputPathObj.open(mode = 'r', newline = '', encoding = inputEncoding)
 	content = f.read()
 	f.close()
+
+	for replaceWord in replaceWords:
+		content = content.replace(replaceWord[0], replaceWord[1])
+
+	cc = OpenCC(method)
 	content = cc.convert(content)
 	if method in ['hk2s', 't2s', 'tw2s', 'tw2sp']:
 		QDict = quoteDict
@@ -36,13 +40,14 @@ def convertZhFile(inputPathObj, outputPathObj, method = 'tw2sp', convertQuote = 
 	for q in QDict:
 		content = content.replace(q, QDict[q])
 	outputPathObj.parent.mkdir(parents = True, exist_ok = True)
-	fout = outputPathObj.open(mode = 'w', newline = '')
+	fout = outputPathObj.open(mode = 'w', newline = '', encoding = outputEncoding)
 	fout.write(content)
 	fout.close()
 
-def batchConvertZhFile(inputPath, outputPath, extension = 'txt', method = 'tw2sp', convertQuote = True):
+def batchConvertZhFile(inputPath, outputPath, inputEncoding = 'utf8', outputEncoding = 'utf8', extension = 'txt', method = 'tw2sp', convertQuote = True, replaceWords = []):
 	for p in getFilePaths(inputPath, extension = extension):
 		pout = outputPath.joinpath(p.relative_to(inputPath))
-		convertZhFile(p, pout, method = method, convertQuote = convertQuote)
+		convertZhFile(p, pout, method = method, convertQuote = convertQuote, inputEncoding = 'utf8', outputEncoding = 'utf8', replaceWords = replaceWords)
 
-batchConvertZhFile(Path('input'), Path('output'), extension = ['txt'])
+
+batchConvertZhFile(inputPath = Path('3_i18n/zh_CN/LC_MESSAGES'), outputPath = Path('3_i18n/zh_TW/LC_MESSAGES'), extension = ['po'], method = 's2twp', replaceWords = [['Language: zh_CN', 'Language: zh_TW'], ['自由天堂', '自由港']])

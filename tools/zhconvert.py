ce2c8990201c9108b27870d4f3f105e9b8b7a293
Tom Chen <tomchen.org@gmail.com>
2020-08-20 20:23:55 +0200

    zh cn tw convert msgstr only and not msgctxt

--- tools/zhconvert.py
+++ tools/zhconvert.py
@@ -1,8 +1,13 @@
 # ZhConvert
 
 from pathlib import Path
-from getfilepaths import getFilePaths
 from opencc import OpenCC
+import polib
+import time
+
+
+start = time.time()
+
 
 quoteDict = {
 '「': '“',
@@ -10,6 +15,7 @@ quoteDict = {
 '『': '‘',
 '』': '’'
 }
+
 
 # hk2s: Traditional Chinese (Hong Kong standard) to Simplified Chinese
 # s2hk: Simplified Chinese to Traditional Chinese (Hong Kong standard)
@@ -21,33 +27,37 @@ quoteDict = {
 # t2tw: Traditional Chinese to Traditional Chinese (Taiwan standard)
 # tw2s: Traditional Chinese (Taiwan standard) to Simplified Chinese
 # tw2sp: Traditional Chinese (Taiwan standard) to Simplified Chinese (with phrases)
-def convertZhFile(inputPathObj, outputPathObj, method = 'tw2sp', convertQuote = True, inputEncoding = 'utf8', outputEncoding = 'utf8', replaceWords = []):
-	f = inputPathObj.open(mode = 'r', newline = '', encoding = inputEncoding)
-	content = f.read()
-	f.close()
+method = 's2twp'
+cc = OpenCC(method)
+convertQuote = True
+replaceWords = [['自由天堂', '自由港'], ['恩洛斯', '安罗斯'], ['贾丹姆', '贾达密'], ['咔', '咯'], ['～', '-']]
+
+
+po = polib.pofile(Path('3_i18n/zh_CN/LC_MESSAGES/mm678.po'))
+for entry in po:
 
 	for replaceWord in replaceWords:
-		content = content.replace(replaceWord[0], replaceWord[1])
+		outputContent = entry.msgstr.replace(replaceWord[0], replaceWord[1])
 
-	cc = OpenCC(method)
-	content = cc.convert(content)
-	if method in ['hk2s', 't2s', 'tw2s', 'tw2sp']:
-		QDict = quoteDict
-	elif method in ['s2hk', 's2t', 's2tw', 's2twp']:
-		QDict = dict([[v,k] for k,v in quoteDict.items()])
-	else:
-		QDict = {}
-	for q in QDict:
-		content = content.replace(q, QDict[q])
-	outputPathObj.parent.mkdir(parents = True, exist_ok = True)
-	fout = outputPathObj.open(mode = 'w', newline = '', encoding = outputEncoding)
-	fout.write(content)
-	fout.close()
+	outputContent = cc.convert(outputContent)
 
-def batchConvertZhFile(inputPath, outputPath, inputEncoding = 'utf8', outputEncoding = 'utf8', extension = 'txt', method = 'tw2sp', convertQuote = True, replaceWords = []):
-	for p in getFilePaths(inputPath, extension = extension):
-		pout = outputPath.joinpath(p.relative_to(inputPath))
-		convertZhFile(p, pout, method = method, convertQuote = convertQuote, inputEncoding = 'utf8', outputEncoding = 'utf8', replaceWords = replaceWords)
+	if convertQuote:
+		if method in ['hk2s', 't2s', 'tw2s', 'tw2sp']:
+			QDict = quoteDict
+		elif method in ['s2hk', 's2t', 's2tw', 's2twp']:
+			QDict = dict([[v,k] for k,v in quoteDict.items()])
+		else:
+			QDict = {}
+		for q in QDict:
+			outputContent = outputContent.replace(q, QDict[q])
+
+	entry.msgstr = outputContent
 
 
-batchConvertZhFile(inputPath = Path('3_i18n/zh_CN/LC_MESSAGES'), outputPath = Path('3_i18n/zh_TW/LC_MESSAGES'), extension = ['po'], method = 's2twp', replaceWords = [['Language: zh_CN', 'Language: zh_TW'], ['自由天堂', '自由港'], ['恩洛斯', '安罗斯'], ['贾丹姆', '贾达密'], ['咔', '咯'], ['～', '-']])
+po.metadata['Language'] = 'zh_TW'
+po.save(Path('3_i18n/zh_TW/LC_MESSAGES/mm678.po'))
+
+
+
+end = time.time()
+print('Execution time: ' + str(end - start) + ' second')

6a2c767c12e72fbb27d7f1890fc996c6943e607d
Tom Chen <tomchen.org@gmail.com>
2020-08-20 20:36:30 +0200

    fix zhconvert problem

--- tools/zhconvert.py
+++ tools/zhconvert.py
@@ -36,8 +36,10 @@ replaceWords = [['自由天堂', '自由港'], ['恩洛斯', '安罗斯'], ['贾
 po = polib.pofile(Path('3_i18n/zh_CN/LC_MESSAGES/mm678.po'))
 for entry in po:
 
+	outputContent = entry.msgstr
+
 	for replaceWord in replaceWords:
-		outputContent = entry.msgstr.replace(replaceWord[0], replaceWord[1])
+		outputContent = outputContent.replace(replaceWord[0], replaceWord[1])
 
 	outputContent = cc.convert(outputContent)
 
